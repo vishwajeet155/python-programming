0aaa88ad6ff8de4d95fb9a465c6698cba99f6c5b
vishwajeet155 <vishurajputmegasoft597@gmail.com>
2026-07-23 10:15:11 +0530

    Add files via upload

---
+++ length breadth.py
@@ -0,0 +1,9 @@
+L= int(input(' Enter length of rectangle here :'))
+B = int(input(' Enter breadth of rectangle here :'))
+
+Area = (L*B)
+Perimeter = (2*(L*B))
+
+print("Area of rectangle:", Area)
+print("Perimeter of rectangle:", Perimeter)
+                          
