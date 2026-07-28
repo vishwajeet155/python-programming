d2a5826e9231bc1471b994b9846e2d1a682a258d
vishwajeet155 <vishurajputmegasoft597@gmail.com>
2026-07-28 10:19:30 +0530

    Add files via upload

---
+++ Comparision between 2 no.py
@@ -0,0 +1,9 @@
+while True:
+ a=int(input("enter first number"))
+ b=int(input("enter second number"))
+ if a>b:
+    print(a,"is greater than",b)
+ elif b>a:
+    print(b,"is greater than",a)
+ else:
+    print("the given numbers are equal")
