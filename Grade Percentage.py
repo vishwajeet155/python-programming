ec6d6682e34c5025947e2471100f5731e11a05e5
vishwajeet155 <vishurajputmegasoft597@gmail.com>
2026-08-01 10:34:22 +0530

    Add files via upload

---
+++ Grade Percentage.py
@@ -0,0 +1,12 @@
+while True:
+ p=eval(input("enter percentage"))
+ if p>=90:
+    print("the grade of student is A+")
+ elif p>=80:
+    print("the grade of student is A")
+ elif p>=70:
+    print("the grade of student is B")
+ elif p>=60:
+    print("the grade of student is C")
+ else:
+    print("the grade of student is D")
